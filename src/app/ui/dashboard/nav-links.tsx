7dfa77df533b2b0d49cdad10e44a54d79429028b
Ryan M <rsymcd@gmail.com>
2025-08-30 14:58:25 -0400

    updated side nav and made links active

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import {
-  UserGroupIcon,
+  BeakerIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
+  RocketLaunchIcon,
+  ClipboardDocumentCheckIcon,
+  ArchiveBoxIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -13,12 +15,14 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Plan', href: '/dashboard/plan', icon: BeakerIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: 'Do',
+    href: '/dashboard/do',
+    icon: RocketLaunchIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Reflect', href: '/dashboard/reflect', icon: ClipboardDocumentCheckIcon },
+  { name: 'Archive', href: '/dashboard/archive', icon: ArchiveBoxIcon },
 ];
 
 export default function NavLinks() {
@@ -33,9 +37,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-purple-600': pathname === link.href,
               },
             )}
           >
