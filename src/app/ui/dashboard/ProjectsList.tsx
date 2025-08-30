549e3f36e9a73a125f990a06fc72a475f809be1c
Ryan M <rsymcd@gmail.com>
2025-08-30 17:12:38 -0400

    removed line between projects

--- src/app/ui/dashboard/ProjectsList.tsx
+++ src/app/ui/dashboard/ProjectsList.tsx
@@ -9,7 +9,7 @@ export default function ProjectsList({
   tasks: Task[];
 }) {
   return (
-    <ul className="divide-y border-b">
+    <ul>
       {projects.map((p) => {
         const projectTasks = tasks.filter((t) => t.project_id === p.id);
         return (
